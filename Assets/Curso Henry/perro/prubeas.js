16251588b0e0cf3eb9843758fba209a0901ce45b
CristianCamelo <cristiamauri08@gmail.com>
2023-10-13 17:40:01 -0500

    commit Homework 05

--- Assets/Curso Henry/perro/prubeas.js
+++ Assets/Curso Henry/perro/prubeas.js
@@ -1,6 +1,12 @@
-let num = parseInt(prompt("Ingrese un numero"))
-   if ( num.length === 3){
-   return true
-   } else {
-   return false
+function interrumpir(num) { 
+let array = [144,45,78,155,478,54,12]
+let Narray =[]
+for (let i =1; i<=10; i++){
+      num = num+2
+      Narray.push(num)
+      if (num=3){
+         return "Se interrumpió la ejecución"
+      }
    }
+}
+   console.log(interrumpir(1))
